240f99107773a546e6110bd8c16f70ab27f05349
silvante <khamidov.ko@gmail.com>
2025-04-27 21:54:54 +0500

    prisma migrations

---
+++ prisma/migrations/20250427165323_initial_and_user/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "profile" TEXT,
+    "banner" TEXT,
+    "verification" BOOLEAN NOT NULL DEFAULT false,
+    "bio" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
