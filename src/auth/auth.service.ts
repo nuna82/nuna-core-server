492f3bd4cee057263ae1e1bfd2eb2b7b22603830
silvante <khamidov.ko@gmail.com>
2025-04-29 18:44:03 +0500

    just in auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+    private readonly mailerService: MailerService,
+  ) {}
+}
